91e4c217761c8176669454eff9191a5f638b3e05
MehdiaHuamis <sisterlittle64@gmail.com>
2025-12-25 09:39:28 +0500

    Fix file

---
+++ dist/assets/supabaseClient-C9EDZ2j4.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-Di0dEqoi.js";export{e as supabase};
--- src/pages/BuildingDetailPage.jsx
+++ src/pages/BuildingDetailPage.jsx
@@ -5,8 +5,8 @@ import { getBuildingById, findNearestRisers, getBuildings, findRisersOnFloor } f
 import BottomNav from '../components/ui/BottomNav';
 import BackButton from '../components/ui/BackButton';
 import Button from '../components/ui/Button';
-import FloorSelector from '../components/Building/FloorSelector';
-import NearestRiserDisplay from '../components/Building/NearestRiserDisplay';
+import FloorSelector from "../components/Building/FloorSelector";
+import NearestRiserDisplay from "../components/Building/NearestRiserDisplay";
 
 const BuildingDetailPage = () => {
   const { id } = useParams();
